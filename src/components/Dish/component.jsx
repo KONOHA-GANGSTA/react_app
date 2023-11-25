429e3a3f304f119a8ce96a6c64cbabd83b1b4ecb
KONOHA_GANGSTA <piligrim507@gmail.com>
2023-11-25 15:18:30 +0300

    Made counter reusable

--- src/components/Dish/component.jsx
+++ src/components/Dish/component.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Counter } from "../Counter/component";
 
 export const Dish = ({ title }) => {
+  const [amount, setAmount] = useState(0);
   return (
     <div>
       <div>{title}</div>
-      <Counter min={0} max={5} />
+      <Counter
+        number={amount}
+        increment={() => setAmount(amount + 1)}
+        decrement={() => setAmount(amount - 1)}
+        min={0}
+        max={5}
+      />
     </div>
   );
 };
