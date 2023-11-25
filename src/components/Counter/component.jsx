0111d00f7e4d5b30623dc5f223ed6b8f0883c803
KONOHA_GANGSTA <piligrim507@gmail.com>
2023-11-25 16:16:51 +0300

    HW4 done

--- src/components/Counter/component.jsx
+++ src/components/Counter/component.jsx
@@ -1,11 +1,11 @@
 export const Counter = ({ number, increment, decrement, min, max }) => {
   return (
     <div>
-      <button onClick={decrement} disabled={number <= min}>
+      <button type="button" onClick={decrement} disabled={number <= min}>
         -
       </button>
       <div>{number}</div>
-      <button onClick={increment} disabled={number >= max}>
+      <button type="button" onClick={increment} disabled={number >= max}>
         +
       </button>
     </div>
