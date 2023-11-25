5071016f3389c2798e7d1a5eec79b8f56a8300a6
KONOHA_GANGSTA <piligrim507@gmail.com>
2023-11-25 14:59:43 +0300

    Restaurant: filter=>find

--- src/components/Restaurant/component.jsx
+++ src/components/Restaurant/component.jsx
@@ -2,19 +2,19 @@ import { Menu } from "../Menu/component";
 import { Reviews } from "../Reviews/component";
 
 export const Restaurant = ({ restaurants, selectedRestaurant }) => {
+  const restaurant = restaurants.find(
+    ({ name }) => name === selectedRestaurant
+  );
   return (
     <div>
       {!selectedRestaurant && <div>Выберите ресторан</div>}
-      {selectedRestaurant &&
-        restaurants
-          .filter(({ name }) => name === selectedRestaurant)
-          .map((restaurant) => (
-            <div>
-              <h1>{restaurant.name}</h1>
-              <Menu dishes={restaurant.menu} />
-              <Reviews reviews={restaurant.reviews} />
-            </div>
-          ))}
+      {selectedRestaurant && (
+        <div>
+          <h1>{restaurant.name}</h1>
+          <Menu dishes={restaurant.menu} />
+          <Reviews reviews={restaurant.reviews} />
+        </div>
+      )}
     </div>
   );
 };
