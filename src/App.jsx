77377cde9c054a42d8eae6adbac5cd7d197e5f53
KONOHA_GANGSTA <piligrim507@gmail.com>
2023-12-02 12:01:05 +0300

    restaurantpage + header + footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,6 @@
-import { useState } from "react";
-import { Restaurant } from "./components/Restaurant/component";
-import { RestaurantsTabs } from "./components/RestaurantsTabs/component";
-import { restaurants } from "./constants/mock";
-import "./styles.module.css";
+import { RestaurantPage } from "./pages/RestaurantPage/component";
+import "./styles/styles.module.css";
 
 export const App = () => {
-  const [selectedRestaurant, changeSelectedRestaurant] = useState(null);
-  return (
-    <>
-      <RestaurantsTabs
-        restaurants={restaurants}
-        selected={selectedRestaurant}
-        changeSelected={changeSelectedRestaurant}
-      />
-      <Restaurant
-        restaurants={restaurants}
-        selectedRestaurant={selectedRestaurant}
-      />
-    </>
-  );
+  return <RestaurantPage />;
 };
