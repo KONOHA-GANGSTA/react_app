cbaa7a798651527dbb0ce898c0ec11e7f1889876
KONOHA_GANGSTA <piligrim507@gmail.com>
2023-12-08 14:58:21 +0300

    fix merge conflicts

--- src/components/Restaurant/component.jsx
+++ src/components/Restaurant/component.jsx
@@ -5,17 +5,13 @@ import styles from "./styles.module.css";
 import cn from "classnames";
 import { selectRestaurantNameById } from "../../redux/features/entities/restaurants/selectors";
 
-
 export const Restaurant = ({ selectedRestaurantId, className }) => {
   const name = useSelector((state) =>
     selectRestaurantNameById(state, selectedRestaurantId)
   );
   return (
     <div className={cn(styles.layout, className)}>
-      {!selectedRestaurantId && (
-        <div className={styles.noselect}>Выберите ресторан</div>
-      )}
-      {selectedRestaurantId && (
+      {selectedRestaurantId ? (
         <div className={styles.layout}>
           <h1 className={styles.title}>{name}</h1>
           <Menu restaurantId={selectedRestaurantId} />
